cb94ed849d0d9e60f7e66aedb798478e1ad5b5d7
paulopaul7 <preethampaul9@gmail.com>
2020-08-16 18:12:15 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New</title>
+    <!--bootstrap css-->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+    <h1>hello there!</h1>
+
+    <div class="container">
+        <div class="row">
+        <div class="col">
+        hello world
+        </div>
+        </div>
+    </div>
+
+
+
+
+    <!--bootstrap jquery-->
+    <script src="./js/jquery-3.5.1.min.js"></script>
+    <!--bootstrap js-->
+    <script src="./js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
